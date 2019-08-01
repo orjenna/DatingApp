52b609bf336f793ccabe05dc03ef14b8cc126131
Malakyan <omalakyan@libertyglobal.com>
2019-08-01 22:03:07 +0200

    Initial Commit

---
+++ Dating.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Dating.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values {get; set; }
+        
+    }
+}
